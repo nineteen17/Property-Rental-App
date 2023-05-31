91e3fb2568364dd177e03d48d8e0f7f9ae56bc68
nineteen17 <102508550+nineteen17@users.noreply.github.com>
2023-05-31 16:23:21 +1200

    updated the client url to fetch from deployed url instead of localhost

--- client/src/api/getHello.tsx
+++ client/src/api/getHello.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const getHello = async () => {
   try {
-    const response = await axios.get("http://localhost:4000/api/hello");
+    const response = await axios.get("http://20.248.224.164/api/hello");
     return response.data;
   } catch (error) {
     throw new Error("Failed to fetch hello");
