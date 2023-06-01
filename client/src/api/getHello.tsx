a25b5e1ba50f0830d5ad6dd2cb7b1c5cb37cf2ce
nineteen17 <102508550+nineteen17@users.noreply.github.com>
2023-06-01 12:27:05 +1200

    changed the backend url to localhost:4000/

--- client/src/api/getHello.tsx
+++ client/src/api/getHello.tsx
@@ -4,7 +4,7 @@ export const getHello = async () => {
   const baseUrl = import.meta.env.VITE_BACKEND_URL;
 
   try {
-    const response = await axios.get(`${baseUrl}/api/hello`);
+    const response = await axios.get(`${baseUrl}api/hello`);
     return response.data;
   } catch (error) {
     throw new Error("Failed to fetch hello");
