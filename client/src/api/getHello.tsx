87a5b57ae0683d17b21bffb33710fa0a20c3f77d
nineteen17 <102508550+nineteen17@users.noreply.github.com>
2023-06-01 14:36:15 +1200

    client env has local and real urlfrom the backend

--- client/src/api/getHello.tsx
+++ client/src/api/getHello.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const getHello = async () => {
-  const baseUrl = import.meta.env.VITE_BACKEND_URL;
+  const baseUrl = import.meta.env.VITE_BACKEND_URL || import.meta.env.VITE_LOCAL_URL;
 
   try {
     const response = await axios.get(`${baseUrl}/api/hello`);
