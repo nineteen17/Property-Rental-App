15d5a41b3fc37661120cc6dcc69058e49776995b
Rainbow <kenesou@gmail.com>
2023-06-02 00:04:58 +1200

    add mongoDB setup

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,20 +1,17 @@
 import express from 'express'
 import propertyRouter from './routes/properties'
+import helloRouter from './routes/properties'
 import cors from 'cors'
 import env from 'dotenv'
-import mongoose from 'mongoose'
 
 env.config()
-
-mongoose
-    .connect(process.env.MONGO_URL as string)
-    .then(() => console.log("DB Connection Successful!"))
-    .catch((err) => console.log(err));
 
 const app = express()
 app.use(express.json())
 app.use(cors())
 
+
+app.use("/api/hello", helloRouter)
 app.use("/api/properties", propertyRouter)
 
 export default app
