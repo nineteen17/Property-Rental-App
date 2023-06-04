0e5f4fe060af14ea647c7bcf01daffe671036590
nineteen17 <102508550+nineteen17@users.noreply.github.com>
2023-06-04 13:46:14 +1200

    added sass

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,11 @@
 import { QueryClient, QueryClientProvider } from "react-query";
 const queryClient = new QueryClient();
+import "./App.scss";
+import { Routes, Route } from "react-router-dom";
 import Header from "./components/shared/header/Header";
 import Footer from "./components/shared/footer/Footer";
 import Home from "./components/routed/home/Home";
-import { Routes, Route } from "react-router-dom";
+
 import Login from "./components/routed/login/Login";
 
 function App() {
