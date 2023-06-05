bd08dd51910bd046b9b3ab3f783df4eaca8d827c
Rainbow <kenesou@gmail.com>
2023-06-05 12:51:38 +1200

    update listing and filter template

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,21 @@
 import { QueryClient, QueryClientProvider } from "react-query";
-import Hello from "./components/Hello";
 const queryClient = new QueryClient();
+import Header from "./components/shared/header/Header";
+import Footer from "./components/shared/footer/Footer";
+import Home from "./components/routed/home/Home";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <>
-      <Hello />
-      </>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </div>
     </QueryClientProvider>
   );
 }
-
 export default App;
---
+++ client/src/components/routed/properties/SingleListing.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SingleListing = () => {
+  return (
+    <div>SingleListing</div>
+  )
+}
+
+export default SingleListing
